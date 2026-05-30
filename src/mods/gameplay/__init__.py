47e4aa37efe8a57895119859ce4a1c658a11759d
matvejpozidaev50-web <matvejpozidaev50@gmail.com>
2026-05-30 20:18:06 +0300

    Update gameplay mods package with new mods

---
+++ src/mods/gameplay/__init__.py
@@ -0,0 +1,7 @@
+"""Gameplay mods package."""
+
+from src.mods.gameplay.night_speed_mod import NightSpeedMod
+from src.mods.gameplay.skip_nights_mod import SkipNightsMod
+from src.mods.gameplay.god_mode_mod import GodModeMod
+
+__all__ = ["NightSpeedMod", "SkipNightsMod", "GodModeMod"]
